a86341f93090a90458771cb5dc957b5687600474
TianQi <tq_email@qq.com>
2018-07-24 12:27:30 +0800

    测试代码

--- src/test/java/cn/sql/cloud/sql/SQLServiceJUnit.java
+++ src/test/java/cn/sql/cloud/sql/SQLServiceJUnit.java
@@ -1,5 +1,8 @@
 package cn.sql.cloud.sql;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
@@ -7,12 +10,13 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import cn.sql.cloud.entity.JDBCInfo;
-import cn.sql.cloud.entity.QueryResult;
-import cn.sql.cloud.entity.UpdateResult;
+import cn.sql.cloud.entity.JDBC;
+import cn.sql.cloud.entity.meta.Column;
+import cn.sql.cloud.entity.meta.IMetaData;
+import cn.sql.cloud.entity.meta.Table;
 import cn.sql.cloud.exception.JDBCNotFoundException;
 import cn.sql.cloud.jdbc.JDBCManager;
-import cn.sql.cloud.jdbc.SQLRunner;
+import cn.sql.cloud.service.MetaDataService;
 import cn.sql.cloud.service.SQLService;
 
 /**
@@ -28,31 +32,51 @@ public class SQLServiceJUnit {
 	@Resource
 	private SQLService sqlService;
 	
+	@Resource
+	private MetaDataService metaDataService;
+	
 	//设置数据源
 	@org.junit.Before
-	public void initJdbcInfo() throws JDBCNotFoundException {
-		String username = "TianQi", jdbcName = "mysql";
-		JDBCInfo jdbcInfo = new JDBCInfo();
-		jdbcInfo.setDatabase("jchc_supplier");
-		jdbcInfo.setHost("172.20.1.199");
+	public void initMySQL() throws JDBCNotFoundException {
+		String username = "SQLCloud", jdbcName = "SQLCloud-MySQL";
+		JDBC jdbcInfo = new JDBC();
+		jdbcInfo.setDatabase("sqlcloud");
+		jdbcInfo.setHost("127.0.0.1");
 		jdbcInfo.setName(jdbcName);
-		jdbcInfo.setUsername("jchctest");
-		jdbcInfo.setPassword("Jchc20170321");
+		jdbcInfo.setUsername("root");
+		jdbcInfo.setPassword("root");
 		jdbcInfo.setPort(3306);
 		jdbcInfo.setSqlType(SQLType.MYSQL);
-		JDBCManager.addJdbcInfo(jdbcInfo, username);
-		JDBCManager.holderJdbcInfo(username, jdbcName);
+		JDBCManager.addJdbc(jdbcInfo, username);
+		JDBCManager.holderJdbc(username, jdbcName);
 	}
 
 	@Test
-	public void executeQuery() {
-		//show databases
-		System.err.println("=======================================");
-		UpdateResult updateResult = SQLRunner.executeUpdate("use jchc_oa");
-		System.out.println(updateResult);
-		QueryResult queryResult = SQLRunner.executeMapQuery("select * from b_jchc_file");
-		System.err.println(queryResult);
-		System.err.println("=======================================");
+	public void getDatabases() throws SQLException {
+		List<? extends IMetaData> list = metaDataService.topTreeNodes();
+		for(IMetaData database:list) {
+			System.out.println(database.getName());
+		}
 	}
-
+	
+	@Test
+	public void getTables() throws SQLException {
+		List<Table> list = metaDataService.tables("%");
+		for(Table item:list) {
+			System.out.println(item);
+		}
+	}
+	
+	@Test
+	public void getColumns() throws SQLException {
+		List<Column> list = metaDataService.columns("jchc_supplier", "b_jchc_supplier_baseinfo");
+		for(Column item:list) {
+			System.out.println(item);
+		}
+	}
+	
+	//打印
+	static void println(Object value) {
+		System.out.println(value);
+	}
 }
